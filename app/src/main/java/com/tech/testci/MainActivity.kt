5e4f62cd2f6894b4c69a7eb81aeed3352ef92166
SaikrishnaDev2022 <110665561+SaikrishnaDev2022@users.noreply.github.com>
2023-01-07 23:41:19 +0800

    take fastlane ss

--- app/src/main/java/com/tech/testci/MainActivity.kt
+++ app/src/main/java/com/tech/testci/MainActivity.kt
@@ -2,10 +2,21 @@ package com.tech.testci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var txt : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txt = findViewById(R.id.txt)
+        button = findViewById(R.id.btn)
+        button.setOnClickListener {
+            txt.text = "Heey how are you doing?"
+
+        }
     }
 }
